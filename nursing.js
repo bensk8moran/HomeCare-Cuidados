211151d423a708065c89654b84d3d826a1fd398e
Benito Moran <97987427+bensk8moran@users.noreply.github.com>
2023-08-05 01:10:00 -0300

    Add files via upload

---
+++ nursing.js
@@ -0,0 +1,27 @@
+
+function enfermagem(){
+    let msgcuidador = document.getElementById("cuIdosos");
+    let fisio = document.getElementById("fisio");
+    if ("msgcuidador"){
+        console.log(msgcuidador)
+        msgcuidador.innerHTML = 'Profissionais qualificados para oferecer os melhores atendimentos e necessidade de nossos clientes';
+    }else {
+    return
+}
+}
+function fiso(){
+    let fisio = document.getElementById("fisio");
+    if("fisio"){  
+        console.log(fisio)  
+        fisio.innerText = 'Profissionais qualificados nos cuidados na reabilitação e pós-operatório'
+}
+function tecEnf(){
+    let tecEnf = document.getElementById("tecEnf");
+    if("tecEnf"){
+        console.log(tecEnf)
+        tecEnf.innerHTML = 'Técnicos em Enfermagem qualificados e atuantes no mercado'
+    } else {
+        return
+    }
+}
+}
